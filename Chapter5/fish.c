9f31d95467cadc7815aea5b5f9e2e6a4d5fe31fe
Humberto Perez <betomperez@gmail.com>
2017-09-25 12:43:12 -0300

    Modified to use typedef

--- Chapter5/fish.c
+++ Chapter5/fish.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
-struct exercise {
+typedef struct {
 	const char *description;
 	float duration;
-};
+}exercise;
 
-struct meal {
+typedef struct {
 	const char *ingredients;
 	float weight;
-};
+}meal;
 
-struct preferences {
-	struct meal food;
-	struct exercise exercise;
-};
+typedef struct {
+	meal food;
+	exercise exercise;
+}preferences;
 
-struct fish {
+typedef struct {
 	const char *name;
 	const char *species;
 	int teeth;
 	int age;
-	struct preferences care;
-};
+	preferences care;
+}fish;
 
-void catalog(struct fish f)
+void catalog(fish f)
 {
 	printf("%s is a %s with %i teeth. He is %i\n", f.name, f.species, f.teeth, f.age);
 }
 
-void label(struct fish f)
+void label(fish f)
 {
 	printf("Name: %s\nSpecies: %s\n%i years old, %i teeth\n", f.name, f.species, f.age, f.teeth);
 	printf("Feed with %.2f lbs of %s and allow %s for %.2f", f.care.food.weight, f.care.food.ingredients, f.care.exercise.description, f.care.exercise.duration);
@@ -36,7 +36,7 @@ void label(struct fish f)
 
 int main()
 {
-	struct fish snappy = {"Snappy", "Piranha", 69, 4, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
+	fish snappy = {"Snappy", "Piranha", 69, 4, {{"meat", 0.2}, {"swim in the jacuzzi", 7.5}}};
 	catalog(snappy);
 	label(snappy);
 	return 0;
