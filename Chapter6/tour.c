38ce2aaa6bd3771c222749633e180e8a3751f439
Humberto Perez <betomperez@gmail.com>
2017-09-26 10:38:21 -0300

    Malloc features

--- Chapter6/tour.c
+++ Chapter6/tour.c
@@ -1,32 +1,31 @@
-#include <stdio.h>
+#include "tour.h"
 
-typedef struct island {
-	char *name;
-	char *opens;
-	char *closes;
-	struct island *next;
-}island;
+int main()
+{
+	char name[80];
+	fgets(name, 80, stdin);
+	island *p_island0 = create(name);
+	fgets(name, 80, stdin);
+	island *p_island1 = create(name);
+	p_island0->next = p_island1;
+	
+	display(p_island0);
+}
 
 void display(island *start)
 {
 	island *i = start;
 	for(; i != NULL; i = i->next){
-		printf("Name: %s\nOpen: %s - %s\n", i->name, i->opens, i->closes);
+		printf("Name: %sOpen: %s - %s\n", i->name, i->opens, i->closes);
 	}
 }
 
-int main()
+island* create(char *name)
 {
-	island amity = {"Amity", "9:00", "17:00", NULL};
-	island craggy = {"Craggy", "9:00", "17:00", NULL};
-	island isla_nublar = {"Isla Nublar", "9:00", "17:00", NULL};
-	island shutter = {"Shutter", "9:00", "17:00", NULL};
-	amity.next = &craggy;
-	craggy.next = &isla_nublar;
-	isla_nublar.next = &shutter;
-	island skull = {"Skull", "9:00", "17:00", NULL};
-	isla_nublar.next = &skull;
-	skull.next = &shutter;
-	
-	display(&amity);
+	island *i = malloc(sizeof(island));
+	i->name = strdup(name);
+	i->opens = "9:00";
+	i->closes = "17:00";
+	i->next = NULL;
+	return i;
 }
---
+++ Chapter6/tour.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+typedef struct island {
+	char *name;
+	char *opens;
+	char *closes;
+	struct island *next;
+}island;
+
+void display(island *start);
+island* create(char *name);
+
